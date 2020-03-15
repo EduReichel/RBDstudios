7b2125919af2d5295b0985c2e0f55b20c02e0c4a
Eduardo <eduardoreichel@gmail.com>
2020-03-15 19:25:37 -0300

    Correciones Seba y JS

--- js/changeImage.js
+++ js/changeImage.js
@@ -1,26 +1,29 @@
-function changeImage() {
-                    
-   /* var path = document.getElementById("1").alt;
-    prompt(path);*/
-    
-    if (document.getElementById('1').alt=="1") 
-    {
-        document.getElementById("principal").src = "img/artMuseum/5.jpg";
-    }
-    else if (document.getElementById("2").src == "../img/artMuseum/4.jpg")
-    {
-       var prueba = document.getElementById("principal");
-        prueba.src = "img/artMuseum/6.jpg";
-    }
-    else if (document.getElementById("3").src == "img/artMuseum/7.jpg")
-    {
-        document.getElementById("principal").src = "img/artMuseum/7.jpg";
-    }
-    else if (document.getElementById("4").src == "img/artMuseum/8.jpg")
-    {
-        document.getElementById("principal").src = "img/artMuseum/8.jpg";
-    }
-    else {
-        document.getElementById("principal").src = "img/artMuseum/error.jpg";
-    }
+/* Lo trabajé con "var" y no con "const o let" 
+porque no sé si estás claro con el standar de ES6 */ 
+
+var imgPrincipal = document.querySelector("#principal");
+var minis = document.querySelectorAll(".imagenes-chicas img");
+var opacity = 0.5;
+
+// Asigna la opacidad de la primera imagen de las minis
+minis[0].style.opacity = opacity;
+
+//Esto si es ES6. Tambien lo puede lograr con un for loop
+minis.forEach(img => img.addEventListener("click", imgActivada));
+
+function imgActivada(e){
+  // Resetea el opacity en todos los minis
+  minis.forEach(img => (img.style.opacity = 1));
+
+  /*Cambia la imagen actual a la fuente de la imagen en la que se hizo clic*/ 
+  imgPrincipal.src = e.target.src;
+
+  // Añade al bonus cheto 
+  imgPrincipal.classList.add("fade-in");
+
+  // Remueve la clase fade in del bonus cheto a los 5ms. 
+  setTimeout(() => imgPrincipal.classList.remove("fade-in"), 500);
+
+  // Cambia la opacidad al valor de la variable opacity dentro del bloque de la funcion. 
+  e.target.style.opacity = opacity;
 }
